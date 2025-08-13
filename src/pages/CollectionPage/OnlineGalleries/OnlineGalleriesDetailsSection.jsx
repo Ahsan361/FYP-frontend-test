de5fbc1ef645241d6cea7247acb127dde37a0065
mzainnasir010 <221412@students.au.edu.pk>
2025-08-13 12:34:40 +0500

    create OnlineGalleriesDetailsSection Page

---
+++ src/pages/CollectionPage/OnlineGalleries/OnlineGalleriesDetailsSection.jsx
@@ -0,0 +1,114 @@
+import { CssBaseline, ThemeProvider, Typography, Box, Grid } from "@mui/material";
+import { lightTheme, darkTheme } from '../../../styles/theme';
+import { useSelector } from "react-redux";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+//my custom
+import Button from "../../../components/ui/Button";
+import { Padding } from "@mui/icons-material";
+
+function OnlineGalleriesDetailsSection(){
+    const darkMode = useSelector((state) => state.theme.darkMode);
+    const theme = darkMode ? darkTheme : lightTheme;
+    return(
+        <ThemeProvider theme= {theme}>
+        <CssBaseline/>
+        <Grid container >
+            {/* {Left Column} */}
+            <Grid size={{sm:14, md:8}}>
+                <Box
+                    sx={{
+                        height:"100%",
+                        backgroundColor: theme.palette.background,  
+                        display:"flex",
+                        flexDirection: "column", 
+                        justifyContent:"left",
+                        p:{xs:2, sm:4, md:8},
+                        pr:{xs:2, sm:8, md:20},
+                        color:theme.palette.mode === "dark"? theme.palette.text.primary:"#101010",
+                        gap:{xs:2, sm:4},
+
+                    }}
+                >
+                    <Typography variant="h1"
+                        sx={{
+                            fontSize: { xs: "1.5rem", sm: "2rem", md: "3rem" },
+                            lineHeight: 1.2,
+                            textAlign: { xs: "center", sm: "left" },
+                        }}
+                    >
+                        Get closer to the British Museum collection and immerse yourself in two million years of history, across six continents.
+                    </Typography>                    
+                    <Typography variant="body1"
+                        sx={{
+                            fontSize: { xs: "0.9rem", sm: "1rem", md: "1.5rem" },
+                            lineHeight: 1.5,
+                            textAlign: { xs: "left", sm: "left" },
+                        }}>
+                        Collection online allows access to almost five million objects in more than two million records. High definition images can be enlarged and examined in detail which will enable you to view the incredible workmanship on the Royal Game of Ur, or the intricate carving on this African hunting horn – just a few of the thousands of highlights to discover. 
+                    </Typography>
+                    <Typography variant="body1"
+                        sx={{
+                            fontSize: { xs: "0.9rem", sm: "1rem", md: "1.5rem" },
+                            lineHeight: 1.5,
+                        }}
+                    >
+                        Enjoy exploring the collection – from some of the earliest objects created by humankind to works by contemporary artists. Or choose from the curated collections below, which reveal the fascinating stories that transcend time.
+                    </Typography>
+                    <Typography
+                        variant="div"
+                        sx={{
+                            fontSize: { xs: "0.9rem", sm: "1rem", md: "1.5rem" },
+                            lineHeight: 1.5,
+                            paddingLeft:10
+                        }}
+                    >
+                        <ul>
+                            <li>China</li>
+                            <li>Americs</li>
+                            <li>Death and Memory</li>
+                        </ul>
+                    </Typography>
+                </Box>
+            </Grid>
+            {/* {Right Column} */}
+            <Grid size={{xs: 14, sm: 14, md: 4}}>
+                <Box
+                    sx={{
+                        height: "100%",
+                        backgroundColor: theme.palette.background,  
+                        display: "flex",
+                        flexDirection: "column", 
+                        justifyContent: "flex-start",
+                        p: { xs: 2, sm: 4, md: 8 },
+                        pr: { xs: 2, sm: 8, md: 20 },
+                        color: theme.palette.mode === "dark" ? theme.palette.text.primary : "#101010",
+                        gap: { xs: 2, sm: 4 },
+                    }}
+                >
+                    {/* Buttons */}
+                    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                        <Button variant="outlined">
+                            <Typography sx={{fontSize: {xs:"0.9rem", sm:"1rem", md:"1.5rem"}}}>
+                                Explore the galleries
+                            </Typography>
+                        </Button>
+                        <Button variant="outlined">
+                            <Typography sx={{fontSize: {xs:"0.9rem", sm:"1rem", md:"1.5rem"}}}>
+                                How objects come into the collection
+                            </Typography>
+                        </Button>
+                        <Button variant="outlined">
+                            <Typography sx={{fontSize: {xs:"0.9rem", sm:"1rem", md:"1.5rem"}}}>
+                                The British Museum story
+                            </Typography>
+                        </Button>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    </ThemeProvider>
+    )
+}
+
+export default OnlineGalleriesDetailsSection;
